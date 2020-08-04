e0eb14b2128156a05898a36551af0f806e78bcb3
Chris <paul19992012@gmail.com>
2020-08-03 21:32:49 -0500

    Examen

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Escolastico'`, () => {
+  it(`should have as title 'ej-escolastico'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Escolastico');
+    expect(app.title).toEqual('ej-escolastico');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Escolastico app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('ej-escolastico app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Escolastico';
+  title = 'FrontEnd Certificación I - 7614';
 }
